de4be0b1b6ab35a43c6b9bc7575f964df503da74
Tobias van Beek <t.vanbeek@tjvb.nl>
2021-04-15 23:06:11 +0200

    Add the setup for the first tests

---
+++ tests/WebhookDataStoringTest.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TJVB\GitLabWebhooks\Tests;
+
+class WebhookDataStoringTest
+{
+    /**
+     * @test
+     */
+    public function weStoreTheWebhookData(): void
+    {
+        $this->markTestIncomplete('TODO');
+// setup / mock
+
+        // run
+
+        // verify/assert
+    }
+
+    /**
+     * @test
+     */
+    public function weGetAnExceptionIfWeHaveInvallidJson(): void
+    {
+        $this->markTestIncomplete('TODO');
+// setup / mock
+
+        // run
+
+        // verify/assert
+    }
+
+    /**
+     * @test
+     */
+    public function weSeeTheDifferenceBetweenAnSystemHookAndNormalHook(): void
+    {
+        $this->markTestIncomplete('TODO');
+        // setup / mock
+
+        // run
+
+        // verify/assert
+    }
+
+    /**
+     * @test
+     */
+    public function weStoreTheObjectKind(): void
+    {
+        $this->markTestIncomplete('TODO');
+        // setup / mock
+
+        // run
+
+        // verify/assert
+    }
+
+    /**
+     * @test
+     */
+    public function weStoreTheEventData(): void
+    {
+        $this->markTestIncomplete('TODO');
+        // setup / mock
+
+        // run
+
+        // verify/assert
+    }
+
+    /**
+     * @test
+     */
+    public function weDispatchTheHookHandling(): void
+    {
+        $this->markTestIncomplete('TODO');
+        // setup / mock
+
+        // run
+
+        // verify/assert
+    }
+}
---
+++ tests/WebhookReceiverTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TJVB\GitLabWebhooks\Tests;
+
+class WebhookReceiverTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function weCanStoreAValidRequest(): void
+    {
+        $this->markTestIncomplete('TODO');
+        // setup / mock
+
+        // run
+
+        // verify/assert
+    }
+
+    /**
+     * @test
+     */
+    public function weGetABadRequestIfWeHaveInvallidData(): void
+    {
+        $this->markTestIncomplete('TODO');
+        // setup / mock
+
+        // run
+
+        // verify/assert
+    }
+
+    /**
+     * @test
+     */
+    public function weGetAnInternalServerErrorIfWeHaveAnUnkownError(): void
+    {
+        $this->markTestIncomplete('TODO');
+        // setup / mock
+
+        // run
+
+        // verify/assert
+    }
+}
